9a478a7a28c1016dacb438d24fd21c9957ec83aa
HongYeonLee <125536259+HongYeonLee@users.noreply.github.com>
2024-09-11 22:39:50 +0900

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
+++ 240911/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 bool isPrime(int n){
+    
+    if (n == 1){
+        return false;
+    }
+
     for (int i = 2; i < n; i++){
-        if (n%i == 0 || n == 1){
+        if (n%i == 0){
             return false;
         }
     }
