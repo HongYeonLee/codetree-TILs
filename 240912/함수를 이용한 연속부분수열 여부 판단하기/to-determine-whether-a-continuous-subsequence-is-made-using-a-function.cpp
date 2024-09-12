82f2f172924b60c5abf48de1e27de15a0e5b2cb1
HongYeonLee <125536259+HongYeonLee@users.noreply.github.com>
2024-09-12 15:21:55 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240912/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
+++ 240912/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.cpp
@@ -2,14 +2,19 @@
 #include <vector>
 using namespace std;
 
+bool isSame(int n){
+    for (int i = 0; i < b.size(); i++){
+        if (a[i + n] != b[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
 bool isSubsequence(vector<int>& a, vector<int>& b){
-    for (int i = 0; i < a.size(); i++){
-        for (int j = 0; j < b.size(); j++){
-            if(b[j] == a[i]){
-                if(b[j+1] == a[i+1]){
-                    return true;
-                }
-            }
+    for (int i = 0; i <= a.size() - b.size(); i++){
+        if(isSame(i)){
+            return true;
         }
     }
     return false;
@@ -21,23 +26,17 @@ int main() {
     cin >> n1 >> n2;
     vector<int> a(n1);
     vector<int> b(n2);
-    int input;
 
     for (int i = 0; i < a.size(); i++){
-        cin >> input;
-        a[i] = input;
+        cin >> a[i];
     }
 
     for (int i = 0; i < b.size(); i++){
-        cin >> input;
-        b[i] = input;
+        cin >> b[i];
     }
 
-    //연산
-    bool ans = isSubsequence(a, b);
-
-    //출력 
-    if(ans){
+    //연산 & 출력
+    if(IsSubsequence(a, b)){
         cout << "Yes";
     }
     else{
