17579aae0f6eaf05fe4347ec5b406f18da88556a
HongYeonLee <125536259+HongYeonLee@users.noreply.github.com>
2024-09-13 16:44:36 +0900

    Title: palindrome 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240913/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+bool isPalindrome(vector<char>& a){
+    vector<char> reverse;
+    for (int i = a.size() - 1; i >= 0; i--){
+        reverse.push_back(a[i]);
+    }
+    for (int i = 0; i < a.size(); i++){
+        if(a[i] != reverse[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+int main() {
+    //입력
+    vector<char> a;
+    string s;
+    
+    for (char c : s){
+        a.push_back(c);
+    }
+
+    //연산 & 출력
+    if (isPalindrome(a)){
+        cout << "Yes";
+    }
+    else{
+        cout << "No";
+    }
+
+    return 0;
+}
